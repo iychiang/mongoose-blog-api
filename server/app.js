3161567b9aa664c0702e212365b445887bbfe1af
Irene <iychiangsd@gmail.com>
2018-06-19 11:15:41 -0700

    Added circleci .yml file

--- server/app.js
+++ server/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+if (process.env.NODE_ENV != 'production') {
 mongoose.connect('mongodb://localhost/my-blog');
+} else {
+mongoose.connect(process.env.MONGODB_URI)
+}
+
 mongoose.Promise = Promise;
 
 const app = express();
